efa9a8a7d9d568dea272c485586350c02cade68b
0masa9 <nktmsk2003@i.softbank.jp>
2024-02-02 19:40:52 +0900

    JS内で動くようになった
    解説通り

--- JS_数当てゲーム/app.js
+++ JS_数当てゲーム/app.js
@@ -1,5 +1,19 @@
-let number = prompt('数字を入力してください')
+let maximum = parseInt(prompt('数字を入力してください'));
 
-number=parseInt(number)
+while(!maximum){
+    maximum=parseInt(prompt('エラーが起きました。有効な数字を入力してください'))
+}
 
-Math.floor(Math.random()*maximum)+1
+const targetNum = Math.floor(Math.random()*maximum)+1;
+console.log(targetNum);
+let guess = parseInt(prompt('1つに数字を決めました。数字を当ててみてください'));
+
+while(guess != targetNum){
+    if (guess > targetNum){
+        guess = parseInt(prompt('その数字より小さいです！もう一度当ててみてください：'));
+    }else{
+        guess = parseInt(prompt('その数字より大きいです！もう一度当ててみてください：'));
+    }
+}
+
+console.log("正解！");
